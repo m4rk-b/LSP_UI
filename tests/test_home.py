444984566dd94e4a9d56c08e404492b03e46cbea
Mark Gil Bejasa <bejasa.markgilc@gmail.com>
2024-10-14 23:04:18 +0800

    Update utilities.py.

--- tests/test_home.py
+++ tests/test_home.py
@@ -11,6 +11,7 @@ class HomeTest(BaseCase):
     def initialize_test(self):
         self.maximize_window()
         self.open("https://mingle-cqa-portal.cqa.inforcloudsuite.com/v2/LCLQA_AX1/59b159d4-4a16-422e-bedf-d57a5b0f139c")
+        #TODO: Use environment variables for username and password
         self.send_keys("//input[@id='username']", "")
         self.send_keys("//input[@id='pass']", "")
         self.click("//button[@title = 'Sign in']")
@@ -19,23 +20,61 @@ class HomeTest(BaseCase):
         self.click("//div[@data-mgcompname = 'AEDroplist']//div[@aria-label = 'Open']")
         self.click(f"//div[@data-mgcompname = 'AEDroplist']//ul/table//td[contains(text(), '{accounting_entity}')]", "xpath")
 
-    def test_settings_page(self):
-        # Menu
-        self.utils.switch_to_menu_frame()
-        self.click("//ul[@id = 'accordion2']//a[contains(text(), 'Settings')]", "xpath", 5)
-
+    def settings_contact_master(self, test_text):
         self.utils.switch_to_cards_frame()
 
         self.click("//div//h4[contains(text(), 'Contact Master')]")
 
-        self.switch_to_default_content()
-        self.switch_to_frame("LSP_31_59b159d4-4a16-422e-bedf-d57a5b0f139c")
+        self.utils.switch_to_main_frame()
         self.utils.wait_for_loading_invisible()
+        #ADD
         self.utils.click_button("AddButton")
-
-        self.utils.input_text("NameChildFormEdit", "TESTNAME")
-
+        self.utils.input_text("NameChildFormEdit", test_text)
         self.utils.click_button("SaveButton")
+        self.utils.message_box("OK")
+        #MODIFY
+        self.utils.table(test_text)
+        self.utils.click_button("ModifyButton")
+        self.utils.clear_input("NameChildFormEdit")
+        self.utils.input_text("NameChildFormEdit", f"MOD{test_text}")
+        self.utils.click_button("SaveButton")
+        self.utils.message_box("OK")
+        #DELETE
+        self.utils.click_button("DeleteButton")
+        self.utils.table(f"MOD{test_text}")
+        self.utils.message_box("OK")
+        self.utils.click_button("SaveButton")
+        self.utils.message_box("OK")
+
+        self.utils.close_settings_form("Contacts Maintenance")
+
+    def default_einvoice_type(self):
+        print("No script added yet")
+
+    def dte_default_code(self):
+        print("No script added yet")
+
+    def export_invoices_customs_information(self, test_text):
+        self.utils.switch_to_cards_frame()
+
+        self.click("//div//h4[contains(text(), 'Export Invoices - Customs Information')]")
+
+        self.utils.switch_to_main_frame()
+        self.utils.wait_for_loading_invisible()
+        self.utils.user_control_buttons("add-button")
+        self.utils.switch_to_main_frame()
+        self.utils.combobox_input("FormCollectionGrid", "InvoiceIDComboBox")
+        self.utils.user_control_buttons("save-button")
+
+
+    def settings_page(self):
+        # Menu
+        self.utils.switch_to_menu_frame()
+        self.click("//ul[@id = 'accordion2']//a[contains(text(), 'Settings')]", "xpath", 5)
+
+        # self.settings_contact_master("TESTNAME")
+        self.export_invoices_customs_information("TEST")
+
 
     def test_home_page(self):
         self.initialize_test()
@@ -47,6 +86,6 @@ class HomeTest(BaseCase):
 
         self.select_ae_to_test("CL_M3")
 
-        self.test_settings_page()
+        self.settings_page()
 
 
--- utils/utilities.py
+++ utils/utilities.py
@@ -3,6 +3,7 @@ class Utils:
         self.util = util
 
     def switch_to_main_frame(self):
+        self.util.switch_to_default_content()
         self.util.switch_to_frame("LSP_31_59b159d4-4a16-422e-bedf-d57a5b0f139c")
         self.util.wait(2)
 
@@ -18,6 +19,12 @@ class Utils:
         iframe_name = "mg_frame" + self.util.get_attribute("//div[@data-mgcompname = 'SubscriptionListUserControl']", "id")
         self.util.switch_to_frame(iframe_name)
 
+    def switch_to_user_control_button_frame(self):
+        self.util.switch_to_default_content()
+        self.util.switch_to_frame("LSP_31_59b159d4-4a16-422e-bedf-d57a5b0f139c")
+        iframe_name = "mg_frame" + self.util.get_attribute("//div[@data-mgcompname = 'maintenanceToolbarUserControl']", "id")
+        self.util.switch_to_frame(iframe_name)
+
     def wait_for_loading_invisible(self):
         is_loading = True
         while is_loading:
@@ -26,10 +33,50 @@ class Utils:
                 is_loading = False
                 self.util.wait(5)
 
+    def message_box(self, button_name):
+        is_message_box_element = self.util.is_element_present("//div[@data-mgcompname = 'MG.MessageBox' and @aria-hidden = 'false']")
+        if is_message_box_element:
+            self.util.click(f"//div[@data-mgcompname = 'MG.MessageBox' and @aria-hidden = 'false']//a[@data-mgcompnamevalue = '{button_name}']")
+            self.util.wait(0.5)
+
     def click_button(self, mg_compname):
         self.util.click(f"//div[@data-mgcompname = '{mg_compname}']//button")
         self.util.wait(1)
 
-    def input_text(self, mg_companame, input_text):
-        self.util.send_keys(f"//div[@data-mgcompname = '{mg_companame}']//input", input_text)
+    def user_control_buttons(self, button_id):
+        self.switch_to_user_control_button_frame()
+        self.util.click(f"//div[@class = 'buttonset']//button[@id='{button_id}']")
         self.util.wait(1)
+
+    def input_text(self, mg_compname, input_text):
+        self.util.send_keys(f"//div[@data-mgcompname = '{mg_compname}']//input", input_text)
+        self.util.wait(0.5)
+
+    def combobox_input(self, table_mg_compname, combobox_mg_compname):
+        self.util.click(f"//div[@data-mgcompname = '{combobox_mg_compname}']//div[@aria-label = 'Open']")
+        self.util.wait(1)
+        table_element_id = self.util.get_attribute(f"//div[@data-mgcompname = '{table_mg_compname}']", "id")
+        table_element_values = self.util.find_elements(f"//div[@id = '{table_element_id}-normal-body']//td/div")
+        combobox_values = self.util.find_elements(f"//div[@data-mgcompname = '{combobox_mg_compname}']//ul//td")
+        select_value = ""
+        table_texts = [table_element_value.text for table_element_value in table_element_values]
+
+        for combobox_value in combobox_values:
+            if combobox_value.text not in table_texts:
+                select_value = combobox_value.text
+                break
+        self.util.click(f"//div[@data-mgcompname = '{combobox_mg_compname}']//ul//td[contains(text(), '{select_value}')]")
+        #TODO: Add a condition if combobox is nullable, if yes, add custom text if no value is available in the combobox, if no, skip add, edit, and delete.
+
+    def clear_input(self, mg_compname):
+        self.util.clear(f"//div[@data-mgcompname = '{mg_compname}']//input")
+        self.util.wait(0.5)
+
+    def close_settings_form(self, form_header_text):
+        self.util.click(f"//div[@aria-label = '{form_header_text}']//div[@aria-label = 'Close Window']")
+
+    def table(self, text_to_search):
+        is_text_exist = self.util.is_element_present(f"//div[@data-mgcompname = 'MainGrid']//td/div[contains(text(), '{text_to_search}')]")
+        if is_text_exist:
+            self.util.click(f"//div[@data-mgcompname = 'MainGrid']//td/div[contains(text(), '{text_to_search}')]")
+            self.util.wait(0.5)
